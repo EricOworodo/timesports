fbc259ec0b73220eea8db315057f8ddfc10b072f
EricOworodo <eric.oworodo@gmail.com>
2023-09-01 12:47:28 +0100

    updates

--- src/components/Bottom_Contact.jsx
+++ src/components/Bottom_Contact.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function Bottom_Contact() {
   return (
-    <div style={{display:'flex', justifyContent:'center', textAlign:'left',fontSize:14, fontWeight:300}}>
+    <div style={{display:'flex', justifyContent:'left', textAlign:'left',fontSize:14, fontWeight:300}}>
         <ul style={{listStyleType:'none'}}>
             <p style={{fontSize:18, fontWeight:500}}>Contact Us</p>
            
